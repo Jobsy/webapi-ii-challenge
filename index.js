6d9299adc36fbb6cf8ee4666fccc2a9a8cc71004
Oluwajoba <oluwajobabello@gmail.com>
2019-10-29 18:00:01 +0100

    makes a POST when the client makes post request to /api/posts / index.js

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
-
+const dB = require("./data/db");
 
 server.get("/", (req, res) => {
     res.send("hello world");
+})
+
+server.post("/api/posts", (req, res) => {
+    const posts = req.body;
+    const { title, content } = req.body;
+    const { url } = req;
+    if (!title || !content) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    dB.insert(posts)
+        .then(() => {
+            res.status(200).json({ postedContent: posts, url: url, operation: "POST" })
+        })
+        .catch(() => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
 })
 
 const port = process.env.PORT || 7000;
