9b920b510ff12517af183740a688194c84b81de8
Oluwajoba <oluwajobabello@gmail.com>
2019-10-30 08:55:25 +0100

    import react / useState / useEffect / axios

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState([]);
+  useEffect(() => {
+  axios
+    .get(`http://localhost:7000/api/posts`)
+    .then(response => {
+      setState(response.data);
+      
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
+  console.log("ppp: ", state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>webapi-II-challenge App</h1>
+        {state.map((post) => {
+console.log("iiii: ", post)
+        })}
+      </>
     </div>
   );
 }
