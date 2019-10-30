0b054b6c09dfe5354279e0ea8162bb8d22c4b6cb
Oluwajoba <oluwajobabello@gmail.com>
2019-10-30 09:06:25 +0100

    map through states / display content to the dom / App.js

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -7,24 +7,27 @@ function App() {
 
   const [state, setState] = useState([]);
   useEffect(() => {
-  axios
-    .get(`http://localhost:7000/api/posts`)
-    .then(response => {
-      setState(response.data);
-      
-    })
-    .catch(error => {
-      console.error(error);
-    });
+    axios
+      .get(`http://localhost:7000/api/posts`)
+      .then(response => {
+        setState(response.data);
+
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }, []);
-  console.log("ppp: ", state)
+
   return (
     <div className="App">
       <>
         <h1>webapi-II-challenge App</h1>
-        {state.map((post) => {
-console.log("iiii: ", post)
-        })}
+        {state.map((post) => (
+          <li>
+            Title: {post.title},
+          Content: {post.contents}
+          </li>
+        ))}
       </>
     </div>
   );
